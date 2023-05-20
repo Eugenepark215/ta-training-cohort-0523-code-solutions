0502d39538fd0a84cb76f9f81cd2a313f550a7c1
Eugene Park <epark215@gmail.com>
2023-05-20 01:31:56 +0000

    Completed square and shape

--- es6-classes/shape.js
+++ es6-classes/shape.js
@@ -1,1 +1,13 @@
 /* exported Shape */
+class Shape {
+  constructor(area, perimeter) {
+    this.values = [area, perimeter];
+  }
+
+  describe() {
+    return `The area is ${this.values[0]} and the perimeter is ${this.values[1]}`;
+  }
+}
+
+const newShape = new Shape(10, 20);
+console.log(newShape.describe());
--- es6-classes/square.js
+++ es6-classes/square.js
@@ -1,2 +1,17 @@
 /* eslint-disable no-undef -- Shape is imported by .html */
 /* exported Square */
+class Square extends Shape {
+  constructor(width) {
+    super(width);
+    this.values = [width];
+  }
+
+  describe() {
+    return `The shape is a Square. The width is ${this.values}, the area is ${
+      this.values ** 2
+    }, and the perimeter is ${this.values * 4}`;
+  }
+}
+
+const newSquare = new Square(12);
+console.log(newSquare.describe());
