ed8747ae33909de3de1cbb3f2aed60ece68e613b
Eugene Park <epark215@gmail.com>
2023-05-22 20:10:45 +0000

    openAccount & getAccount

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,28 @@
 /* exported Bank */
+class Bank {
+  constructor() {
+    this.nextAccountNumber = 1;
+    this.accounts = [];
+  }
+
+  openAccount(holder, balance) {
+    if (balance <= 0 || !Number.isInteger(balance)) {
+      return null;
+    } else {
+      const newAccount = new Account(this.nextAccountNumber, holder);
+      newAccount.deposit(balance);
+      this.accounts.push(newAccount);
+      this.nextAccountNumber++;
+      return newAccount.number;
+    }
+  }
+
+  getAccount(number) {
+    for (let j = 0; j < this.accounts.length; j++) {
+      if (this.accounts[j].number === number) {
+        return this.accounts[j];
+      }
+    }
+    return null;
+  }
+}
