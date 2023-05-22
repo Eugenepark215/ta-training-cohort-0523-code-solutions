f6768848daa8897b99a7f062f5501a752d4f3804
Eugene Park <epark215@gmail.com>
2023-05-22 19:53:54 +0000

    completed account

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -1,1 +1,40 @@
 /* exported Account */
+class Account {
+  constructor(number, holder, transactions) {
+    this.number = number;
+    this.holder = holder;
+    this.transactions = [];
+  }
+
+  deposit(amount) {
+    if (amount <= 0 || !Number.isInteger(amount)) {
+      return false;
+    } else {
+      const depositAmount = new Transaction('deposit', amount);
+      this.transactions.push(depositAmount);
+      return true;
+    }
+  }
+
+  withdraw(amount) {
+    if (amount <= 0 || !Number.isInteger(amount)) {
+      return false;
+    } else {
+      const withdrawAmount = new Transaction('withdrawal', amount);
+      this.transactions.push(withdrawAmount);
+      return true;
+    }
+  }
+
+  getBalance() {
+    let balance = 0;
+    for (let i = 0; i < this.transactions.length; i++) {
+      if (this.transactions[i].type === 'deposit') {
+        balance += this.transactions[i].amount;
+      } else {
+        balance -= this.transactions[i].amount;
+      }
+    }
+    return balance;
+  }
+}
