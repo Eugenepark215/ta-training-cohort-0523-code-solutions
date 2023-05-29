d2aa5cca008593521fb850725adcb709d3e0dae7
Eugene Park <epark215@gmail.com>
2023-05-29 23:18:12 +0000

    completed react-components

---
+++ react-components/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { CustomButton } from './CustomButton';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomButton />
+    </div>
+  );
+}
+
+export default App;
