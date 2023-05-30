433d44e3f7b084b09678260be40267ba8a593825
Eugene Park <epark215@gmail.com>
2023-05-30 21:03:33 +0000

    Completed

--- react-hot-button-component/my-app/src/hot-button-component.js
+++ react-hot-button-component/my-app/src/hot-button-component.js
@@ -1,10 +1,45 @@
 import { useState } from 'react';
 
-export function HotButton({ color }) {
+export function HotButton() {
   const [isClicked, incrementClick] = useState(0);
 
+  function handleIncrementClick() {
+    incrementClick(isClicked + 1);
+  }
+
+  if (isClicked >= 18) {
+    return (
+      <button className="" onClick={handleIncrementClick}>
+        Hot Button
+      </button>
+    );
+  } else if (isClicked >= 15) {
+    return (
+      <button className="yellow" onClick={handleIncrementClick}>
+        Hot Button
+      </button>
+    );
+  } else if (isClicked >= 12) {
+    return (
+      <button className="orange" onClick={handleIncrementClick}>
+        Hot Button
+      </button>
+    );
+  } else if (isClicked >= 9) {
+    return (
+      <button className="red" onClick={handleIncrementClick}>
+        Hot Button
+      </button>
+    );
+  } else if (isClicked >= 6) {
+    return (
+      <button className="blue-violet" onClick={handleIncrementClick}>
+        Hot Button
+      </button>
+    );
+  }
   return (
-    <button className={color} onClick={() => incrementClick(isClicked + 1)}>
+    <button className="purple" onClick={handleIncrementClick}>
       Hot Button
     </button>
   );
