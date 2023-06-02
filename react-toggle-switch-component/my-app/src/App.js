307f2a9c4b243db2ce9987353eb32ccceca85e2e
Eugene Park <epark215@gmail.com>
2023-06-02 05:32:21 +0000

    Completed

--- react-toggle-switch-component/my-app/src/App.js
+++ react-toggle-switch-component/my-app/src/App.js
@@ -1,5 +1,18 @@
+import { useState } from 'react';
 function App() {
-  return <div></div>;
+  const [clicked, setClicked] = useState('OFF');
+
+  function Toggle() {
+    return clicked === 'OFF' ? setClicked('ON') : setClicked('OFF');
+  }
+  return (
+    <div className="display-flex">
+      <div className={`background background-${clicked}`}>
+        <button className={` switch switch-${clicked}`} onClick={Toggle} />
+      </div>
+      <p className="text">{clicked}</p>
+    </div>
+  );
 }
 
 export default App;
