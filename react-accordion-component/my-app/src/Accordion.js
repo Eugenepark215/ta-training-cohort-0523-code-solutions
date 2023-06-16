157ee4dd3755831662b583ad8e972c3e814a2cf5
Eugene Park <epark215@gmail.com>
2023-06-16 04:20:23 +0000

    Completed:

--- react-accordion-component/my-app/src/Accordion.js
+++ react-accordion-component/my-app/src/Accordion.js
@@ -1,11 +1,25 @@
-// import { useState } from "react";
+import { useState } from 'react';
 export function Accordion(props) {
-  // const [handleClick, setClick] = useState('');
+  const [handleClick, setClick] = useState('');
+
+  function setToggle(e) {
+    if (handleClick === e.target.textContent) {
+      setClick('');
+    } else {
+      setClick(e.target.textContent);
+    }
+  }
+
   const topicText = props.array.map((element, index) => (
     <li key={index}>
-      <div className="topic">{element.topic}</div>
-      <div className="text">{element.text}</div>
+      <div className="topic" onClick={setToggle}>
+        {element.topic}
+      </div>
+      {element.topic === handleClick && (
+        <div className="text">{element.text}</div>
+      )}
     </li>
   ));
+
   return <ul>{topicText}</ul>;
 }
