fea6f3ee66860fa14b5fe762ffc70c4557a5de79
Eugene Park <epark215@gmail.com>
2023-06-21 23:24:32 +0000

    completed exceptions.js

--- es6-async-await/exceptions.js
+++ es6-async-await/exceptions.js
@@ -5,52 +5,47 @@ import fetch from './fetch.js';
 const startTime = Date.now();
 const elapsed = () => `${Math.round((Date.now() - startTime) / 1000)}s -`;
 
-function throwOnce() {
+async function throwOnce() {
   // Note: In the `catch` we are logging just `error.message` for illustration
   // purposes. In actual code you will want to log the entire error so that
   // you get the stack trace.
-  return fetch('foo', true)
-    .then((msg) => console.log(elapsed(), 'throwOnce:', msg))
-    .catch((error) =>
-      console.log(elapsed(), 'throwOnce Error:', error.message)
-    );
+  try {
+    const data = await fetch('foo', true);
+    console.log(elapsed(), 'throwOnce:', data);
+  } catch (error) {
+    console.log(elapsed(), 'throwOnce Error:', error.message);
+  }
 }
 
-function throwSeveral() {
+async function throwSeveral() {
   // Note: In the `catch` we are logging just `error.message` for illustration
   // purposes. In actual code you will want to log `error` so that
   // you get the stack trace.
-  return fetch('foo1', true)
-    .then((msg) => {
-      console.log(elapsed(), 'throwSeveral1:', msg);
-      return fetch('foo2', true);
-    })
-    .then((msg) => {
-      console.log(elapsed(), 'throwSeveral2:', msg);
-      return fetch('foo3', true);
-    })
-    .then((msg) => console.log(elapsed(), 'throwSeveral3:', msg))
-    .catch((error) =>
-      console.log(elapsed(), 'throwSeveral Error:', error.message)
-    );
+  try {
+    const data1 = await fetch('foo1', true);
+    console.log(elapsed(), 'throwSeveral1:', data1);
+    const data2 = await fetch('foo2', true);
+    console.log(elapsed(), 'throwSeveral2:', data2);
+    const data3 = await fetch('foo3/bar.html');
+    console.log(elapsed(), 'fetchSeveral3:', data3);
+  } catch (error) {
+    console.log(elapsed(), 'throwSeveral Error:', error.message);
+  }
 }
 
-function throwChained() {
-  return fetch('foo-chain', true)
-    .then((msg1) => {
-      console.log(elapsed(), 'throwChained1:', msg1);
-      return fetch(msg1, true);
-    })
-    .then((msg2) => {
-      console.log(elapsed(), 'throwChained2:', msg2);
-      return fetch(msg2, true);
-    })
-    .then((msg3) => console.log(elapsed(), 'throwChained3:', msg3))
-    .catch((error) =>
-      console.log(elapsed(), 'throwChained Error:', error.message)
-    );
+async function throwChained() {
+  try {
+    const data1 = await fetch('foo-chain/bar.html', true);
+    console.log(elapsed(), 'throwChained1:', data1);
+    const data2 = await fetch(data1);
+    console.log(elapsed(), 'throwChained2:', data2);
+    const data3 = await fetch(data2);
+    console.log(elapsed(), 'throwChained3:', data3);
+  } catch (error) {
+    console.log(elapsed(), 'throwChained Error:', error.message);
+  }
 }
 
-throwOnce()
-  .then(() => throwSeveral())
-  .then(() => throwChained());
+await throwOnce();
+await throwSeveral();
+await throwChained();
