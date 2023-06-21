a99f61a3b48b0604d6172aa43f5b634fbf4c2d7b
Eugene Park <epark215@gmail.com>
2023-06-21 23:07:34 +0000

    completed await.js

--- es6-async-await/await.js
+++ es6-async-await/await.js
@@ -5,38 +5,41 @@ import fetch from './fetch.js';
 const startTime = Date.now();
 const elapsed = () => `${Math.round((Date.now() - startTime) / 1000)}s -`;
 
-function fetchOnce() {
-  return fetch('foo/bar.html').then((msg) =>
-    console.log(elapsed(), 'fetchOnce:', msg)
-  );
+async function fetchOnce() {
+  try {
+    const data = await fetch('foo/bar.html');
+    console.log(elapsed(), 'fetchOnce:', data);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function fetchSeveral() {
-  return fetch('foo1/bar.html')
-    .then((msg) => {
-      console.log(elapsed(), 'fetchSeveral1:', msg);
-      return fetch('foo2/bar.html');
-    })
-    .then((msg) => {
-      console.log(elapsed(), 'fetchSeveral2:', msg);
-      return fetch('foo3/bar.html');
-    })
-    .then((msg) => console.log(elapsed(), 'fetchSeveral3:', msg));
+async function fetchSeveral() {
+  try {
+    const data1 = await fetch('foo1/bar.html');
+    console.log(elapsed(), 'fetchSeveral1:', data1);
+    const data2 = await fetch('foo2/bar.html');
+    console.log(elapsed(), 'fetchSeveral2:', data2);
+    const data3 = await fetch('foo3/bar.html');
+    console.log(elapsed(), 'fetchSeveral3:', data3);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function fetchChained() {
-  return fetch('foo-chain/bar.html')
-    .then((msg1) => {
-      console.log(elapsed(), 'fetchChained1:', msg1);
-      return fetch(msg1);
-    })
-    .then((msg2) => {
-      console.log(elapsed(), 'fetchChained2:', msg2);
-      return fetch(msg2);
-    })
-    .then((msg3) => console.log(elapsed(), 'fetchChained3:', msg3));
+async function fetchChained() {
+  try {
+    const data1 = await fetch('foo-chain/bar.html');
+    console.log(elapsed(), 'fetchChained1:', data1);
+    const data2 = await fetch(data1);
+    console.log(elapsed(), 'fetchChained2:', data2);
+    const data3 = await fetch(data2);
+    console.log(elapsed(), 'fetchChained3:', data3);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-fetchOnce()
-  .then(() => fetchSeveral())
-  .then(() => fetchChained());
+await fetchOnce();
+await fetchSeveral();
+await fetchChained();
