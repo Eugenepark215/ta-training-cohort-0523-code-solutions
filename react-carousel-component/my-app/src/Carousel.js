400108583c54fb94b9143c6fb5a3e67a44e162db
Eugene Park <epark215@gmail.com>
2023-06-24 02:58:59 +0000

    added chevron functionality

--- react-carousel-component/my-app/src/Carousel.js
+++ react-carousel-component/my-app/src/Carousel.js
@@ -1,13 +1,32 @@
 import { useState } from 'react';
+
 export function Carousel(props) {
   const [active, setActive] = useState(0);
-  function handleIndex(e) {
+
+  function handleActive(e) {
     setActive(parseInt(e.target.id));
   }
+
+  function chevron(e) {
+    if (e.target.className === 'fa-solid fa-chevron-left') {
+      if (active === 0) {
+        setActive(props.imgArray.length - 1);
+      } else {
+        setActive(active - 1);
+      }
+    } else {
+      if (active === props.imgArray.length - 1) {
+        setActive(0);
+      } else {
+        setActive(active + 1);
+      }
+    }
+  }
+
   return (
     <div class="row margin-top">
       <div class="column-one-third">
-        <i class="fa-solid fa-chevron-left"></i>
+        <i class="fa-solid fa-chevron-left" onClick={chevron} />
       </div>
       <div class="column-three-fifths">
         <img src="imgsPokemon/007.png" alt="Squirtle" />
@@ -22,14 +41,14 @@ export function Carousel(props) {
                 <button
                   className={`${colorIndex}`}
                   id={index}
-                  onClick={handleIndex}></button>
+                  onClick={handleActive}></button>
               </div>
             );
           })}
         </div>
       </div>
       <div class="column-one-third">
-        <i class="fa-solid fa-chevron-right"></i>
+        <i class="fa-solid fa-chevron-right" onClick={chevron} />
       </div>
     </div>
   );
