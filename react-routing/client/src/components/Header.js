2b17234a48738838fa041fc03eeb95fa884f10d1
Eugene Park <epark215@gmail.com>
2023-06-28 00:27:22 +0000

    completed

--- react-routing/client/src/components/Header.js
+++ react-routing/client/src/components/Header.js
@@ -7,18 +7,16 @@ export default function Header(props) {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow-sm">
         <div className="navbar-collapse">
           <ul className="navbar-nav mr-auto">
-            {/* TODO: Make these links to About and Catalog, with className "title" */}
             <li className="nav-item nav-link">
               <Link to="about">About</Link>
             </li>
             <li className="nav-item nav-link">
-              <Link to="catalog">Catalog</Link>
+              <Link to="/">Catalog</Link>
             </li>
           </ul>
         </div>
       </nav>
-      {/* Render the Outlet here. */}
-      <Outlet />
+      {<Outlet />}
     </div>
   );
 }
